aad2ec1de0f06d3598303a025a5ad22bccf4a2dd
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-03 09:36:15 +0300

    created a get() method for hero's form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,65 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App{
     public static void main(String[] args) {
 
         staticFileLocation("/public");
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "index.hbs");
+            return new ModelAndView(model, "form.hbs");
         },new HandlebarsTemplateEngine());
+
+
+        post("/getSquad/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String cause = request.queryParams("cause");
+            String maxMembers = request.queryParams("maxMembers");
+            Squad squad = new Squad(name, cause, maxMembers);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+        get("squad", (request, response) -> { // URL to show the post form data of squad generated
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Squad> teams = Squad.getInstances();
+            model.put("teams", teams);
+            return new ModelAndView(model, "squadList.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+        get("hero", (request, response) -> { // URL to show the post form data of squad generated
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.all();
+            model.put("heros", hero);
+            return new ModelAndView(model, "heroForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        post("/getHero/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String cause = request.queryParams("cause");
+            String maxMembers = request.queryParams("maxMembers");
+            Squad squad = new Squad(name, cause, maxMembers);
+            return new ModelAndView(model, "successhero.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+
+
+
 
 
     }
